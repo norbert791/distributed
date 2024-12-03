44144439fb6b825663215497cef05f150377309f
Norbert Jaśniewicz <261749@student.pwr.edu.pl>
2024-12-03 02:02:28 +0100

    add mat-vec mult

--- lista2/zad2/matrixVector/MatrixVector.java
+++ lista2/zad2/matrixVector/MatrixVector.java
@@ -12,7 +12,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
-public class MatrixMult {
+public class MatrixVector {
 
     // Mapper Class
     public static class MatrixMapper extends Mapper<Object, Text, Text, Text> {
@@ -23,30 +23,29 @@ public class MatrixMult {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String line = value.toString();
             String[] parts = line.split(",");
-            String matrixName = parts[0];
-            int i = Integer.parseInt(parts[1]);
-            int j = Integer.parseInt(parts[2]);
-            int valueMatrix = Integer.parseInt(parts[3]);
             String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
             String[] splittedName = fileName.split("_");
             int numOfRows = Integer.parseInt(splittedName[1]);
             int numOfCols = Integer.parseInt(splittedName[2].substring(0, splittedName[2].length() - 4));
 
-            if (matrixName.equals("A")) {
-                // Emit keys for A(i, k) where k is the column index in the output
-                for (int k = 0; k < numOfCols; k++) { // assume P = 100 (cols in B)
-                    outputKey.set(i + "," + k);
-                    outputValue.set("A," + j + "," + valueMatrix);
-                    context.write(outputKey, outputValue);
-                }
-            } else if (matrixName.equals("B")) {
-                // Emit keys for B(k, j) where k is the row index in A
-                for (int k = 0; k < numOfRows; k++) { // assume M = 100 (rows in A)
-                    outputKey.set(k + "," + j);
-                    outputValue.set("B," + i + "," + valueMatrix);
-                    context.write(outputKey, outputValue);
-                }
-            }
+            if (parts.length == 2) {
+                int i = Integer.parseInt(parts[0]);
+                int valueMatrix = Integer.parseInt(parts[1]);
+              for (int j = 0; j < numOfRows; j++) {
+                outputKey.set(String.valueOf(j));
+                outputValue.set(i + "," + valueMatrix);
+                context.write(outputKey, outputValue);
+              }
+              return;
+            } 
+
+            int row = Integer.parseInt(parts[0]);
+            int col = Integer.parseInt(parts[1]);
+            int v = Integer.parseInt(parts[2]);
+            outputKey.set(String.valueOf(row));
+            outputValue.set(col + "," + v);
+
+            context.write(outputKey, outputValue);
         }
     }
 
@@ -59,11 +58,11 @@ public class MatrixMult {
 
             for (Text value : values) {
                 String[] parts = value.toString().split(",");
-                    int index = Integer.parseInt(parts[1]);
-                    int temp = map.getOrDefault(index, 1);
-                    map.put(index, temp * Integer.parseInt(parts[2]));
-                    int modified = modifiedMap.getOrDefault(index, 0);
-                    modifiedMap.put(index, modified+1);
+                int index = Integer.parseInt(parts[0]);
+                int temp = map.getOrDefault(index, 1);
+                map.put(index, temp * Integer.parseInt(parts[1]));
+                int modified = modifiedMap.getOrDefault(index, 0);
+                modifiedMap.put(index, modified+1);
             }
 
             int sum = 0;
@@ -84,7 +83,7 @@ public class MatrixMult {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "matrix multiplication");
 
-        job.setJarByClass(MatrixMult.class);
+        job.setJarByClass(MatrixVector.class);
         job.setMapperClass(MatrixMapper.class);
         job.setReducerClass(MatrixReducer.class);
         job.setMapOutputKeyClass(Text.class);
